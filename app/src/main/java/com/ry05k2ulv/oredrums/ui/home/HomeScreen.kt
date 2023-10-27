4ed63675b28215cdf746c846a893e0a8ceae9319
ry05k2ulv <ryosuke.10117@gmail.com>
2023-10-27 18:44:44 +0900

    feat: Add side pane for showing details

--- app/src/main/java/com/ry05k2ulv/oredrums/ui/home/HomeScreen.kt
+++ app/src/main/java/com/ry05k2ulv/oredrums/ui/home/HomeScreen.kt
@@ -1,17 +1,33 @@
 package com.ry05k2ulv.oredrums.ui.home
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyGridItemScope
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -19,7 +35,8 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -31,13 +48,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ry05k2ulv.oredrums.model.DrumsProperty
-import com.ry05k2ulv.oredrums.ui.theme.OreDrumsTheme
 import com.ry05k2ulv.oredrums.utils.flip
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Composable
 internal fun HomeRoute(
@@ -49,7 +67,8 @@ internal fun HomeRoute(
     HomeScreen(
         uiState = uiState,
         onPropertyClick = onPropertyClick,
-        onAddProperty = viewModel::insertProperty
+        onAddProperty = viewModel::insertProperty,
+        onRemoveProperty = viewModel::deletePropertyById
     )
 }
 
@@ -58,16 +77,19 @@ internal fun HomeScreen(
     uiState: HomeUiState,
     onPropertyClick: (id: Int) -> Unit,
     onAddProperty: (title: String) -> Unit,
+    onRemoveProperty: (id: Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(modifier) {
         when (uiState) {
             HomeUiState.Loading -> LoadingScreen()
             is HomeUiState.Success -> SuccessScreen(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize(),
                 properties = uiState.properties,
                 onPropertyClick = onPropertyClick,
-                onAddProperty = onAddProperty
+                onAddProperty = onAddProperty,
+                onRemoveProperty = onRemoveProperty
             )
         }
     }
@@ -78,9 +100,12 @@ private fun SuccessScreen(
     modifier: Modifier,
     properties: List<DrumsProperty>,
     onPropertyClick: (id: Int) -> Unit,
-    onAddProperty: (title: String) -> Unit
+    onAddProperty: (title: String) -> Unit,
+    onRemoveProperty: (id: Int) -> Unit
 ) {
     var showAddPropertyDialog by remember { mutableStateOf(false) }
+
+    var propertyShouldShow by remember { mutableStateOf<DrumsProperty?>(null) }
 
     var selectedSet by remember { mutableStateOf(setOf<Int>()) }
 
@@ -93,23 +118,37 @@ private fun SuccessScreen(
             })
     }
 
-    Box(modifier) {
-        PropertyLazyList(
-            modifier = Modifier.fillMaxSize(),
-            properties = properties,
-            selectMode = selectedSet.isNotEmpty(),
-            selectedSet = selectedSet,
-            onSelectChange = { selectedSet = it },
-            onPropertyClick = onPropertyClick
-        )
+    Row(modifier) {
+        Box(Modifier.weight(1f)) {
+            PropertyLazyList(
+                columns = GridCells.Fixed(if (propertyShouldShow == null) 2 else 1),
+                modifier = Modifier.fillMaxSize(),
+                properties = properties,
+                selectMode = selectedSet.isNotEmpty(),
+                selectedSet = selectedSet,
+                onSelectChange = { selectedSet = it },
+                onDetailsClick = { propertyShouldShow = it },
+                onPropertyClick = onPropertyClick
+            )
 
-        FloatingActionButton(
-            onClick = { showAddPropertyDialog = true },
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp)
-        ) {
-            Icon(imageVector = Icons.Default.Add, contentDescription = "Add property")
+            if (selectedSet.isEmpty()) AddFloatingActionButton(
+                onClick = { onAddProperty("Untitled 1") },
+                modifier = Modifier.align(Alignment.BottomEnd)
+            ) else RemoveFloatingActionButton(
+                onClick = { selectedSet.forEach(onRemoveProperty); selectedSet = setOf() },
+                modifier = Modifier.align(Alignment.BottomEnd)
+            )
+        }
+        if (propertyShouldShow != null) {
+            DetailsPane(
+                property = propertyShouldShow!!,
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .fillMaxHeight()
+                    .animateContentSize(),
+                onEditClick = {},
+                onCloseClick = { propertyShouldShow = null },
+            )
         }
     }
 }
@@ -126,25 +165,30 @@ private fun LoadingScreen(
 
 @Composable
 private fun PropertyLazyList(
+    columns: GridCells,
     modifier: Modifier,
     properties: List<DrumsProperty>,
     selectMode: Boolean,
     selectedSet: Set<Int>,
     onSelectChange: (Set<Int>) -> Unit,
-    onPropertyClick: (id: Int) -> Unit
+    onDetailsClick: (DrumsProperty) -> Unit,
+    onPropertyClick: (id: Int) -> Unit,
 ) {
-    LazyColumn(modifier) {
-        items(properties, { it.id }) {
+    LazyVerticalGrid(columns, modifier) {
+        items(items = properties, { it.id }) {
             PropertyLazyListItem(
                 modifier = Modifier
                     .padding(8.dp, 4.dp)
+                    .height(64.dp)
                     .fillMaxWidth(),
                 property = it,
+                selected = it.id in selectedSet,
                 onClick = {
                     if (selectMode) onSelectChange(selectedSet.flip(it.id))
                     else onPropertyClick(it.id)
                 },
-                onLongClick = { onSelectChange(selectedSet.flip(it.id)) }
+                onLongClick = { onSelectChange(selectedSet.flip(it.id)) },
+                onDetailsClick = { onDetailsClick(it) }
             )
         }
     }
@@ -152,20 +196,103 @@ private fun PropertyLazyList(
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-private fun PropertyLazyListItem(
+private fun LazyGridItemScope.PropertyLazyListItem(
     modifier: Modifier,
     property: DrumsProperty,
+    selected: Boolean,
     onClick: () -> Unit,
-    onLongClick: () -> Unit
+    onLongClick: () -> Unit,
+    onDetailsClick: () -> Unit
 ) {
-    Card(
-        modifier = modifier.combinedClickable(
-            onClick = onClick,
-            onLongClick = onLongClick
-        ),
-        elevation = CardDefaults.elevatedCardElevation()
+    val primaryIfSelected by animateColorAsState(if (selected) MaterialTheme.colorScheme.primary else Color.Transparent)
+    val padding by animateDpAsState(if (selected) 1.dp else 0.dp)
+    Box(modifier) {
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding, padding * 2)
+                .combinedClickable(
+                    onClick = onClick,
+                    onLongClick = onLongClick
+                ),
+            elevation = CardDefaults.elevatedCardElevation(),
+            border = BorderStroke(4.dp, primaryIfSelected)
+        ) {
+            Row(
+                Modifier
+                    .padding(4.dp)
+                    .fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = property.title)
+                Spacer(modifier = Modifier.weight(1f))
+                IconButton(onClick = onDetailsClick) {
+                    Icon(imageVector = Icons.Default.Info, contentDescription = "Info")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AddFloatingActionButton(
+    onClick: () -> Unit,
+    modifier: Modifier,
+) {
+    FloatingActionButton(onClick = onClick, modifier = modifier) {
+        Icon(imageVector = Icons.Default.Add, contentDescription = "Add Property")
+    }
+}
+
+@Composable
+fun RemoveFloatingActionButton(
+    onClick: () -> Unit,
+    modifier: Modifier,
+) {
+    FloatingActionButton(onClick = onClick, modifier = modifier) {
+        Icon(imageVector = Icons.Default.Delete, contentDescription = "Remove Property")
+    }
+}
+
+@Composable
+fun DetailsPane(
+    property: DrumsProperty,
+    modifier: Modifier,
+    onEditClick: () -> Unit,
+    onCloseClick: () -> Unit
+) {
+
+    Column(
+        modifier
+            .fillMaxSize()
     ) {
-        Text(text = property.title)
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(8.dp), horizontalArrangement = Arrangement.End
+        ) {
+            IconButton(onClick = onEditClick) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
+            }
+            IconButton(onClick = onCloseClick) {
+                Icon(imageVector = Icons.Default.ArrowForward, contentDescription = "Close")
+            }
+        }
+        Text(
+            text = property.title,
+            modifier = Modifier.padding(8.dp),
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Text(
+            text = "Created At ${property.createdAt.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)}",
+            modifier = Modifier.padding(8.dp),
+            style = MaterialTheme.typography.titleMedium
+        )
+        Text(
+            text = "Updated At ${property.updatedAt.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)}",
+            modifier = Modifier.padding(8.dp),
+            style = MaterialTheme.typography.titleMedium
+        )
     }
 }
 
